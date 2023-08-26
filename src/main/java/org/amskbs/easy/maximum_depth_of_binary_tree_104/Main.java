263cffa19af6cd2b0d87c9deef034a23c375e26b
amskbs <amskbs@yandex.ru>
2023-08-26 07:21:11 +0600

    104. Maximum Depth of Binary Tree

---
+++ src/main/java/org/amskbs/easy/maximum_depth_of_binary_tree_104/Main.java
@@ -0,0 +1,48 @@
+package org.amskbs.easy.maximum_depth_of_binary_tree_104;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node15 = new TreeNode(15);
+        TreeNode node20 = new TreeNode(20, node15, node7);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode root = new TreeNode(3, node9, node20);
+        System.out.println(main.maxDepth(root));
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return maxDepth(root, 1);
+    }
+
+    public int maxDepth(TreeNode node, int level) {
+        return Math.max(
+                node.left == null ? level : maxDepth(node.left, level + 1),
+                node.right == null ? level : maxDepth(node.right, level + 1)
+        );
+    }
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
