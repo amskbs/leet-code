f2fb3a46a9ca364e8d39641bd2e6e75c43edb0e2
amskbs <amskbs@yandex.ru>
2023-08-26 07:44:30 +0600

    101. Symmetric Tree

---
+++ src/main/java/org/amskbs/easy/symmetric_tree_101/Main.java
@@ -0,0 +1,49 @@
+package org.amskbs.easy.symmetric_tree_101;
+
+public class Main {
+
+    // TODO add test cases!
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+
+
+    public boolean isSymmetric(TreeNode root) {
+        return isMirrorTree(root.left, root.right);
+    }
+
+    private boolean isMirrorTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isMirrorTree(p.left, q.right) && isMirrorTree(p.right, q.left);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
