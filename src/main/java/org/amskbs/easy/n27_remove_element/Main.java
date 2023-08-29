437e6bc6207aeb039568bd6399d442109b8d15e0
amskbs <amskbs@yandex.ru>
2023-08-29 07:34:17 +0600

    27. Remove Element

---
+++ src/main/java/org/amskbs/easy/n27_remove_element/Main.java
@@ -0,0 +1,41 @@
+package org.amskbs.easy.n27_remove_element;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        List<Input> inputs = List.of(
+                new Input(new int[]{3, 2, 2, 3}, 3),
+                new Input(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2)
+        );
+
+        for (Input input : inputs) {
+            int x = main.removeElement(input.array, input.k);
+            System.out.println(x);
+            System.out.println(Arrays.toString(input.array));
+        }
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static class Input {
+        final int[] array;
+        final int k;
+
+        public Input(int[] array, int k) {
+            this.array = array;
+            this.k = k;
+        }
+    }
+}
